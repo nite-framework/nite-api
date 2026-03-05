1b7eb89dc7bcd5a5a779e584504088a12c1cee4a
codeBigInt <elliotlucky99@gmail.com>
2026-03-06 00:27:38 +0100

    Revert to initial commit

--- dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { ContractCtor } from './common-types';
-import { CompiledContract, type Contract } from '@midnight-ntwrk/compact-js';
-export declare function createCompiledContract<TContract extends Contract<any, any>>(tag: string, ContractClass: ContractCtor<TContract>, witnesses: Contract.Witnesses<TContract>, assetsPath: string): CompiledContract.CompiledContract<TContract, Contract.Witnesses<TContract>>;
