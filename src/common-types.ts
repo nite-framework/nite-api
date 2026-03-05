7b4f91c7861c1d848aa7d4b20d93f5b0d2e03e83
codeBigInt <elliotlucky99@gmail.com>
2026-03-06 00:38:41 +0100

    cleanup codebase

--- src/common-types.ts
+++ src/common-types.ts
@@ -1,8 +1,3 @@
 import { type Contract } from "@midnight-ntwrk/compact-js";
 
 export type ContractCtor<TContract extends Contract<any, any>> = new (...args: any[]) => TContract;
-
-export interface LedgerMapItem<T extends object> {
-  key: Uint8Array;
-  item: T;
-}
